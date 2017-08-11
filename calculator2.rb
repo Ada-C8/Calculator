b73841af5fae9ecb44dd137b29e9d79ad076f1b0
skwiens <skwiens@gmail.com>
2017-08-11 09:01:36 -0700

    YAY! It subtracts!
    Forgot a pipe.
    
    Also added comments that should have been done as I was working...

--- calculator2.rb
+++ calculator2.rb
@@ -1,3 +1,4 @@
+#Define mathematical methods
 def add(a, b)
   return a + b
 end
@@ -22,6 +23,7 @@ def modulus(a, b)
   return a % b
 end
 
+#Verify that entry is a number
 def verify_number(number, which)
   until number.to_f.to_s == number || number.to_i.to_s == number
     print "Please enter a numerical value for your #{which} number: "
@@ -30,6 +32,7 @@ def verify_number(number, which)
   return number.to_f
 end
 
+#Determine whether to print a float or integer to the screen based on if it ends in zero (int)
 def float_or_int(number)
   if number.to_s[-1] == "0"
     number = number.to_i
@@ -37,13 +40,19 @@ def float_or_int(number)
   return number
 end
 
+#Wasn't used.... this is a work in progress....Will use gsub to replace the parenthesis with the number.
 def parenth(number)
   if number.to_s[0] == "(" && number.to_s[-1] == ")"
-
+    parenth_computation = number[1..(number.length)]
+    find_numbers(parenth_computation)
+    number = calculate(num1, operation, num2)
   else
+    number = number
   end
+  return number
 end
 
+#Calculates based on operation (Still need to delete extra words since they are replaced in line 108
 def calculate(num1, operation, num2)
   case operation
   when "+", "add", "plus"
@@ -73,14 +82,17 @@ def calculate(num1, operation, num2)
   end
 end
 
+#Prompt user to enter math expression
 puts "Enter your mathematical expression using no spaces: "
 computation = gets.chomp
 
+#Prompts user to enter math expression if they only pressed enter
 while computation == ""
   puts "Enter your mathematical expression using no spaces: "
   computation = gets.chomp
 end
 
+#A hash that uses words as keys and the math symbols as values so that words can be replaced by symbols.
 math_hash = {
   "plus" => "+",
   "add" => "+",
@@ -96,21 +108,26 @@ math_hash = {
   "remainder" => "%"
 }
 
+#Replace math words with math symbols
 math_hash.each do |key, value|
   computation.gsub!(key, value)
 end
 
-math_regex = /-\*|\+|\^|\*|\/|%/
+#Look for the index of the operator
+math_regex = /-|\*|\+|\^|\*|\/|%/
 operation_index = computation =~ math_regex
 
+#Determine the first number, operation, and second number
 num1 = computation[0...operation_index]
 num1 = verify_number(num1, "first")
 operation = computation[operation_index]
 num2 = computation[operation_index+1..-1]
 num2 = verify_number(num2, "second")
 
+#Calculate result
 result = calculate(num1, operation, num2)
 
+#Determine if numbers should be printed as floats or integers.
 num1 = float_or_int(num1)
 num2 = float_or_int(num2)
 result = float_or_int(result)
