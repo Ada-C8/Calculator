e5da7afa1d8a10b334883f01b55b2fe2d1def39b
anderschenders <andreachen.ac@gmail.com>
2017-08-16 20:45:28 -0700

    Update calculator.rb
    Separated methods by functionality (e.g., add, subtract, multiply)

--- calculator.rb
+++ calculator.rb
@@ -1,35 +1,53 @@
-# method to process user input
-def calculator(operation, num1, num2)
-  if operation == "add" || operation == "+"
-    equals_to = num1 + num2
-    equals_to = final_result(equals_to)
-    return "#{num1} + #{num2} = #{equals_to}"
-  elsif operation == "subtract" || operation == "-"
-    equals_to = num1 - num2
-    equals_to = final_result(equals_to)
-    return "#{num1} - #{num2} = #{equals_to}"
-  elsif operation == "multiply" || operation == "*"
-    equals_to = num1 * num2
-    equals_to = final_result(equals_to)
-    return "#{num1} * #{num2} = #{equals_to}"
-  elsif operation == "divide" || operation == "/"
-    # ensure second number is a positive number
-    until num2 > 0
-      puts "Uh oh, please enter a positive number for your second number: "
-      num2 = gets.chomp.to_f
-    end
-    equals_to = num1 / num2
-    equals_to = final_result(equals_to)
-    return "#{num1} / #{num2} = #{equals_to}"
-  elsif operation == "exponential" || operation == "^"
-    equals_to = num1 ** num2
-    equals_to = final_result(equals_to)
-    return "#{num1}^#{num2} = #{equals_to}"
-  else # modulo
-    equals_to = num1 % num2
-    equals_to = final_result(equals_to)
-    return "#{num1} % #{num2} = #{equals_to}"
+# Method to add numbers
+def add(num1, num2)
+# if operation == "add" || operation == "+"
+  equals_to = num1 + num2
+  equals_to = final_result(equals_to)
+  return "#{num1} + #{num2} = #{equals_to}"
+end
+
+# Method to subtract numbers
+def subtact(num1, num2)
+# operation == "subtract" || operation == "-"
+  equals_to = num1 - num2
+  equals_to = final_result(equals_to)
+  return "#{num1} - #{num2} = #{equals_to}"
+end
+
+# Method to multiply numbers
+def multiply(num1, num2)
+# operation == "multiply" || operation == "*"
+  equals_to = num1 * num2
+  equals_to = final_result(equals_to)
+  return "#{num1} * #{num2} = #{equals_to}"
+end
+
+# Method to divide
+def divide(num1, num2)
+# elsif operation == "divide" || operation == "/"
+# ensure second number is a positive number
+  until num2 > 0
+    puts "Uh oh, please enter a positive number for your second number: "
+    num2 = gets.chomp.to_f
   end
+  equals_to = num1 / num2
+  equals_to = final_result(equals_to)
+  return "#{num1} / #{num2} = #{equals_to}"
+end
+
+# Method for exponential
+def exponential(num1, num2)
+# operation == "exponential" || operation == "^"
+  equals_to = num1 ** num2
+  equals_to = final_result(equals_to)
+  return "#{num1}^#{num2} = #{equals_to}"
+end
+
+# Method for modulo
+def modulo(num1, num2)
+  equals_to = num1 % num2
+  equals_to = final_result(equals_to)
+  return "#{num1} % #{num2} = #{equals_to}"
 end
 
 # verify that user input is a number
@@ -59,7 +77,6 @@ print "> "
 operation = gets.chomp
 
 # verify user input
-
 until operation == "add" || operation == "+" || operation == "subtract" || operation == "-" ||
   operation == "multiply" || operation == "*" || operation == "divide" || operation == "/" ||
   operation == "exponential" || operation == "^" || operation == "modulo" || operation == "%"
@@ -82,5 +99,21 @@ num2 = gets.chomp
 # verify user input
 num2 = verify(num2).to_f
 
-result = calculator(operation, num1, num2)
+case operation
+
+when "add", "+"
+  result = add(num1, num2)
+  puts result
+when "subtract", "-"
+  result = subtract(num1, num2)
+when "multiply", "*"
+  result = multiply(num1, num2)
+when "divide", "/"
+  result = divide(num1, num2)
+when "exponential", "^"
+  result = exponential(num1, num2)
+when "modulo", "%"
+  result = modulo(num1, num2)
+end
+
 puts "Final result: #{result}"
